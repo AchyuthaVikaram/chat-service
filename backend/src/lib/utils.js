b6bfec41b8cf7e3a653e05ca0e6a5e810a62da95
Achyutha Vikaram <achyuthavikaram@gmail.com>
2025-04-05 14:23:17 +0530

    authentication is done

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,6 +7,7 @@ export const generateToken = (userId,res)=>{
     const token = jwt.sign({userId},process.env.JWT_SECRET,{
         expiresIn:"7d"
     })
+    console.log(token);
     res.cookie("jwt",token,{
         maxAge : 7*24*60*60*1000,
         httpOnly:true, //prevent XSS attcks
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,54 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
 
-function App() {
-	return <div class="text-3xl font-bold underline">Hello APP</div>;
-}
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+
+const App = () => {
+	const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+
+	console.log({ onlineUsers });
+
+	useEffect(() => {
+		checkAuth();
+	}, [checkAuth]);
+
+	console.log({ authUser });
+
+	if (isCheckingAuth && !authUser)
+		return (
+			<div className="flex items-center justify-center h-screen">
+				<Loader className="size-10 animate-spin" />
+			</div>
+		);
+
+	return (
+		<div>
+			<Navbar />
+
+			<Routes>
+				<Route
+					path="/"
+					element={authUser ? <HomePage /> : <Navigate to="/login" />}
+				/>
+				<Route
+					path="/signup"
+					element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/login"
+					element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+				/>
+			</Routes>
+
+			<Toaster />
+		</div>
+	);
+};
 export default App;
